84e1f179ca57e3a6802ebaae7a0c6473fa5ab1ba
floric <floonweb@yahoo.de>
2016-07-13 13:09:55 +0200

    More fixes:
    compiler set to 1.8
    added initialization
    added Canvas class
    added logging file formats to gitignore
    change JUnit 4 dependency in POM

--- src/main/java/org/htwkvisu/controller/ApplicationController.java
+++ src/main/java/org/htwkvisu/controller/ApplicationController.java
@@ -1,7 +1,29 @@
 package org.htwkvisu.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by floric on 7/12/16.
  */
-public class ApplicationController {
+public class ApplicationController implements Initializable {
+
+    @FXML
+    public Button resetViewButton;
+
+    @FXML
+    public Label messageLabel;
+
+    @FXML
+    public AnchorPane canvasPane;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        messageLabel.setText("TEst");
+    }
 }
--- src/main/java/org/htwkvisu/main/App.java
+++ src/main/java/org/htwkvisu/main/App.java
@@ -2,24 +2,69 @@ package org.htwkvisu.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.logging.*;
+
 /**
- * Hello world!
+ *  Main App
+ *  @author floric
  */
 public class App extends Application {
+
+    private static final String WINDOW_TITLE = "Lifequality Visualization";
+    private static final Point2D WINDOW_SIZE = new Point2D(800, 600);
+    private static final String LOG_FILENAME = "lifequality.log";
+
+    /**
+     * Main method
+     *
+     * @param args Program arguments
+     */
     public static void main(String[] args) {
         Application.launch(args);
     }
 
+    /** Start method, called by main-method, overwritten from Application class
+     *
+     * @param primaryStage Stage of window
+     * @throws Exception Ex
+     */
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/org.htwkvisu.view/ApplicationView.fxml"));
-        Scene scene = new Scene(root, 800, 600);
+        Parent root = FXMLLoader.load(getClass().getResource("/view/ApplicationView.fxml"));
+        Scene scene = new Scene(root, WINDOW_SIZE.getX(), WINDOW_SIZE.getY());
 
-        primaryStage.setTitle("Lifequality Visualization");
+        // application specific initializations
+        initApp();
+
+        primaryStage.setTitle(WINDOW_TITLE);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void initApp() {
+        initLogging();
+    }
+
+    private void initLogging() {
+        try {
+            FileHandler fileHandler = new FileHandler(LOG_FILENAME);
+            fileHandler.setLevel(Level.ALL);
+            fileHandler.setFormatter(new SimpleFormatter());
+
+
+            Logger.getGlobal().addHandler(fileHandler);
+
+            Logger.getGlobal().log(Level.INFO, "Logger initialized!");
+        } catch (IOException e) {
+            Logger.getGlobal().addHandler(new ConsoleHandler());
+            Logger.getGlobal().log(Level.WARNING, "Log file output IO error!");
+        }
+
+
     }
 }
